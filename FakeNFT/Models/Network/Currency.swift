98a255e6a8e25a624667a6dc81bbc231db628c2a
Georgy <georgin2000155@gmail.com>
2023-11-08 14:05:40 +0300

    - Network service added

---
+++ FakeNFT/Models/Network/Currency.swift
@@ -0,0 +1,15 @@
+//
+//  Currency.swift
+//  FakeNFT
+//
+//  Created by Georgy on 08.11.2023.
+//
+
+import Foundation
+
+struct CurrencyModel: Decodable{
+    let title: String
+    let name: String
+    let image: String
+    let id: String
+}
